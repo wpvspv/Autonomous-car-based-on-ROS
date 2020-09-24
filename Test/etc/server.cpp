3f1006b902b4b116af252feb71ba5d005c6fc6d1
wpvspv <wpvspv@gmail.com>
2020-09-24 16:48:14 +0900

    replace server.cpp

--- Test/etc/server.cpp
+++ Test/etc/server.cpp
@@ -11,22 +11,17 @@
 #define MAX_BUF 32
  
 using namespace std;
- 
+
 int main(int argc, char*argv[])
 {
     int server_fd, client_fd;
-    char buffer[BUF_SIZE];
+    char buffer[1];
     struct sockaddr_in server_addr, client_addr;
     char temp[20];
     int msg_size;
     socklen_t len;
- 
-    //    if (argc != 3) {
-    //        printf("Usage : %s <port>\n", argv[0]);
-    //        exit(1);
-    //    }
- 
- 
+    int speed=80;
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {// 
         cout << "Server : Can't open stream socket\n";
@@ -51,31 +46,90 @@ int main(int argc, char*argv[])
         cout << "Server : Can't listening connect.\n";
         exit(0);
     }
+
+    //reset address
     memset(buffer, 0x00, sizeof(buffer));
+
     len = sizeof(client_addr);
     cout << "Server : wating connection request.\n";
+
     client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
     inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
-    cout << "Server : %s client connected.\n", temp;
+    cout << "Server : " << temp << " client connected.\n";
     while (1)
     {
- 
-        if (client_fd < 0)
-        {
-            cout << "Server: accept failed.\n";
-            return 0;
-        }
- 
-        msg_size = read(client_fd, buffer, 1024);
-        cout << buffer << endl;
-        if (!strcmp(buffer, "fin"))
-        {
-            cout << "fin\n";
-            return 0;
-        }
- 
- 
+	cout << "waiting" <<endl;
+	client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
+  	inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
+	recv(client_fd,buffer,BUF_SIZE,0);
+	while(1)
+	{
+		//front
+		if(buffer[0]=='W'){
+			cout << "buffer : " <<  buffer <<endl;		
+			break;
+		}
+
+		//back
+		if(buffer[0]=='X'){
+			cout << "buffer : " <<  buffer <<endl;
+			
+			break;
+		}
+
+		//stop
+		if(buffer[0]=='S'){
+			cout << "buffer : " <<  buffer <<endl;
+			break;
+		}
+
+		//front left
+		if(buffer[0]=='Q'){
+			cout << "buffer : " <<  buffer <<endl;
+			break;
+		}
+
+		//front right
+		if(buffer[0]=='E'){
+			cout << "buffer : " <<  buffer <<endl;
+			break;
+		}
+
+		//back left
+		if(buffer[0]=='Z'){
+			cout << "buffer : " <<  buffer <<endl;
+			break;
+		}
+
+		//back right
+		if(buffer[0]=='C'){
+			cout << "buffer : " <<  buffer <<endl;
+			break;
+		}
+
+		//speed up
+		if(buffer[0]=='U'){
+			cout << "buffer : " <<  buffer <<endl;
+			if(speed>=170) speed=170;			
+			else speed+=5;
+			cout << "speed : " << speed <<endl; 
+			break;
+		}
+
+		//speed down
+		if(buffer[0]=='J'){
+			cout << "buffer : " <<  buffer <<endl;
+			if(speed<=80) speed=80;			
+			else speed-=5;
+			cout << "speed : " << speed <<endl; 
+			break;
+		}
+	}
+
+	//reset address
         memset(buffer, 0x00, sizeof(buffer));
+
+
     }
     close(server_fd);
     return 0;
